dcc96aa54382873a85ca9f302c925fb32d66f6f8
Trilkin <nstoppiello@gmail.com>
2020-09-29 11:32:06 -0400

    Comitted SetupLaz.bat.

---
+++ Scripts/Reference/SetupLaz.bat
@@ -0,0 +1,10 @@
+rem This script will download the latest updater/installer from my GitHub.
+rem This is the only file you should run manually.
+rem If you have any issues, let me know in Discord or open a ticket on my GitHub.
+rem Have fun.
+wget -O https://raw.githubusercontent.com/Trilkin/Project-Lazarus-Bits-and-Bobs/master/Scripts/DownloadLaz.bat
+if exist DownloadLaz.bat DownloadLaz.bat
+echo.
+echo Pausing for errors.
+echo.
+pause
