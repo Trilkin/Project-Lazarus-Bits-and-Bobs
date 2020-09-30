8ff501efc06ca02658cee00673699f684808529d
Trilkin <nstoppiello@gmail.com>
2020-09-30 12:55:23 -0400

    Fixed an error in SetupLaz.bat

--- Scripts/Reference/SetupLaz.bat
+++ Scripts/Reference/SetupLaz.bat
@@ -2,7 +2,7 @@ rem This script will download the latest updater/installer from my GitHub.
 rem This is the only file you should run manually.
 rem If you have any issues, let me know in Discord or open a ticket on my GitHub.
 rem Have fun.
-wget -O https://raw.githubusercontent.com/Trilkin/Project-Lazarus-Bits-and-Bobs/master/Scripts/DownloadLaz.bat
+wget -O DownloadLaz.bat https://raw.githubusercontent.com/Trilkin/Project-Lazarus-Bits-and-Bobs/master/Scripts/DownloadLaz.bat
 if exist DownloadLaz.bat DownloadLaz.bat
 echo.
 echo Pausing for errors.
